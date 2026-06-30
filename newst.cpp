802b8984000fbf6db6096aa4f160a841105406a5
rag-vi <raghvi1610@gmail.com>
2026-06-30 21:26:36 +0530

    tenth commit

---
+++ newst.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+string removeDuplicates(string s)
+{
+    stack<char> st;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (!st.empty() && st.top() == s[i])
+        {
+            st.pop();
+        }
+        else
+        {
+            st.push(s[i]);
+        }
+    }
+
+    string ans = "";
+
+    while (!st.empty())
+    {
+        ans += st.top();
+        st.pop();
+    }
+
+    reverse(ans.begin(), ans.end());
+
+    return ans;
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    cout << removeDuplicates(s);
+
+    return 0;
+}
